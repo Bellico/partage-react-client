b9a7433c44350d95637c9859d24b8b40997d315a
Martin Franck <franck.martin12@hotmail.com>
2020-01-23 23:08:44 +0100

    Modal

--- src/layout/board.page.tsx
+++ src/layout/board.page.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
 import { Menu } from './menu';
 import { TodoGroup } from 'content-types/todo/todo-group';
+import { Modal } from 'core/modal';
 
 const WorkContainer = styled.div`
     background: #f4f6f8;
@@ -15,10 +16,11 @@ const WorkContainer = styled.div`
     }
 `;
 
-export const BoardPage: FunctionComponent = () => (
+export const BoardPage = () => (
     <WorkContainer>
         <Menu />
         <main>
+            <Modal>Hello</Modal>
             <TodoGroup />
         </main>
     </WorkContainer>
